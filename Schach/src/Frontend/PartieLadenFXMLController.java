aa4c9500dbceefe961ed4a0a1cb3d2be3eb74c99
edwrardn <edwrardn@LTEDWRARDN4.mansystems.nl>
2018-05-22 17:07:52 +0200

    Exception in Dialogbox ausgeben(Partie laden  in der Startseite)

--- Schach/src/Frontend/PartieLadenFXMLController.java
+++ Schach/src/Frontend/PartieLadenFXMLController.java
@@ -10,7 +10,10 @@ import Backend.SpielException;
 import Backend.Spielbrett;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -23,7 +26,12 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -77,7 +85,36 @@ public class PartieLadenFXMLController implements Initializable {
                 controller.cleanBoard();
                 controller.initSpielbrett();
             } catch (SpielException ex) {
-                Logger.getLogger(PartieLadenFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Exception Dialog");
+                alert.setHeaderText("Ein Fehler ist aufgetreten");
+
+                // Create expandable Exception.
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                ex.printStackTrace(pw);
+                String exceptionText = sw.toString();
+
+                Label label = new Label("The exception stacktrace was:");
+
+                TextArea textArea = new TextArea(exceptionText);
+                textArea.setEditable(false);
+                textArea.setWrapText(true);
+
+                textArea.setMaxWidth(Double.MAX_VALUE);
+                textArea.setMaxHeight(Double.MAX_VALUE);
+                GridPane.setVgrow(textArea, Priority.ALWAYS);
+                GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+                GridPane expContent = new GridPane();
+                expContent.setMaxWidth(Double.MAX_VALUE);
+                expContent.add(label, 0, 0);
+                expContent.add(textArea, 0, 1);
+
+                // Set expandable Exception into the dialog pane.
+                alert.getDialogPane().setExpandableContent(expContent);
+
+                alert.showAndWait();
             }
         }
         else{
