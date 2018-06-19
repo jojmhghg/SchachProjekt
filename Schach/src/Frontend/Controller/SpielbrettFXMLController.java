f226ab5853d76579f5f02fd5390f2a998a2d1888
timtim <timtim@192.168.44.9>
2018-06-19 18:47:28 +0200

    überprüfung der sitzungsID für methoden im backen die partie beeinflussen

--- Schach/src/Frontend/Controller/SpielbrettFXMLController.java
+++ Schach/src/Frontend/Controller/SpielbrettFXMLController.java
@@ -35,7 +35,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.geometry.Point3D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
